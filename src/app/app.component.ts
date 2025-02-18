0a1f3ad2861f1735fdd80921e79eaa464b32b613
Bhargav <vyasb1503@gmail.com>
2025-02-18 19:25:15 +0530

    prictice  of the Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'SampleProject';
+
+  
+  isDisplay= true;
+  buttonName = 'Hide';
+  items = [1,2,3,4,5,6,7,8,9,10];
+  selectedNum=1;
+
+  students = [
+
+    {
+      id :101,
+      name: 'John',
+         },
+         { 
+          id :102,
+          name: 'Smith',
+         },
+
+  ]
+
+  toggle(): void {
+    this.isDisplay = !this.isDisplay; // Toggles the display state
+    if (this.buttonName === 'Hide') {
+      this.buttonName = 'show';
+    }
+  else {
+    this.buttonName = 'Hide';
+  }
+}
+
 }
